23407aca3cd85c4ee4bc81dfdebc9dcab6b2599e
Cristian Oliveira <contato@cristianoliveira.com.br>
2018-02-18 17:41:14 +0100

    feature: Posts rendering comments

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Post.css';
 
+const Comment = (props) => <div>{ props.text }</div>;
+
 class Post extends Component {
   render() {
     if (!this.props.title) {
@@ -13,8 +15,8 @@ class Post extends Component {
         className={`post ${this.props.selected && 'post--selected'}`}
         onClick={this.props.onClick}>
         <h1>{this.props.title}</h1>
-        <div>{this.props.content}</div>
-        {this.props.comments.map(comment => <Comment {...comment} />)}
+        <p>{this.props.content}</p>
+        {this.props.comments.map((comment, i) => <Comment {...comment} key={i} />)}
       </div>
     );
   }
@@ -23,11 +25,13 @@ class Post extends Component {
 Post.propTypes = {
   selected: PropTypes.bool,
   onClick: PropTypes.func,
+  comments: PropTypes.array,
 };
 
 Post.defaultProps = {
   selected: false,
   onClick: () => {},
+  comments: [],
 };
 
 export default Post;
--- src/components/Post.spec.js
+++ src/components/Post.spec.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 import Post from './Post';
 
-it('renders correctly', () => {
-  const wrapper = renderer.create(<Post />).toJSON();
-  expect(wrapper).toMatchSnapshot();
+describe('Post', () => {
+  describe('when it is empty', () => {
+    // given
+    const props = {};
+
+    it('renders nothing', () => {
+      // when
+      const wrapper = shallow(<Post {...props} />);
+
+      //then
+      expect(wrapper.type()).toBeNull();
+    });
+  });
+
+  describe('when it has post content', () => {
+    it('renders title and post content', () => {
+      // given
+      const props = {
+        title: 'Foobar',
+        content: 'Some awesome text',
+      };
+
+      // when
+      const wrapper = shallow(<Post {...props} />);
+
+      // then
+      expect(wrapper.find('h1')).toHaveLength(1);
+      expect(wrapper.find('p')).toHaveLength(1);
+    });
+  });
+
+  describe('when it has commentaries', () => {
+    it('renders a commentary content', () => {
+      // given
+      const props = {
+        title: 'Foobar',
+        content: 'Some awesome text',
+        comments: [{text: 'Awesome'}],
+      };
+
+      // when
+      const wrapper = shallow(<Post {...props} />);
+
+      // then
+      expect(wrapper.find('Comment')).toHaveLength(1);
+    });
+
+    it('renders more than one commentary', () => {
+      // given
+      const props = {
+        title: 'Foobar',
+        content: 'Some awesome text',
+        comments: [{text: 'Awesome'}, {text: 'Other'}],
+      };
+
+      // when
+      const wrapper = shallow(<Post {...props} />);
+
+      // then
+      expect(wrapper.find('Comment')).toHaveLength(2);
+    });
+  });
 });
